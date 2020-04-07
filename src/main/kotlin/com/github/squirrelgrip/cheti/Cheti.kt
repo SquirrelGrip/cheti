7ea6b00a6154222104d8d85e0d2690865bddb990
Adrian <adrian.richter@gmail.com>
2020-04-07 17:44:22 +0800

    Added ability to load configuration using a template

--- src/main/kotlin/com/github/squirrelgrip/cheti/Cheti.kt
+++ src/main/kotlin/com/github/squirrelgrip/cheti/Cheti.kt
@@ -1,18 +1,29 @@
 package com.github.squirrelgrip.cheti
 
 import com.github.squirrelgrip.cheti.configuration.ChetiConfiguration
+import com.github.squirrelgrip.extensions.file.toReader
+import com.github.squirrelgrip.extensions.io.toReader
 import com.github.squirrelgrip.extensions.json.toInstance
-import java.io.File
-import java.io.InputStream
+import org.apache.velocity.Template
+import org.apache.velocity.VelocityContext
+import org.apache.velocity.runtime.RuntimeSingleton
+import java.io.*
 import java.net.InetAddress
+
 
 object Cheti {
 
-    fun loadConfiguration(file: File): ChetiConfiguration = file.toInstance()
+    fun loadConfiguration(file: File, context: Map<String, String> = emptyMap()): ChetiConfiguration {
+        return loadTemplate(file, context).toInstance()
+    }
 
-    fun loadConfiguration(fileName: String): ChetiConfiguration = loadConfiguration(File(fileName))
+    fun loadConfiguration(fileName: String, context: Map<String, String> = emptyMap()): ChetiConfiguration {
+        return loadConfiguration(File(fileName), context)
+    }
 
-    fun loadConfiguration(inputStream: InputStream): ChetiConfiguration = inputStream.toInstance()
+    fun loadConfiguration(inputStream: InputStream, context: Map<String, String> = emptyMap()): ChetiConfiguration {
+        return loadTemplate(inputStream.toReader(), "cheti", context).toInstance()
+    }
 
     private fun validate(chetiConfiguration: ChetiConfiguration) {
         val errors = chetiConfiguration.validate()
@@ -38,16 +49,50 @@ object Cheti {
         }
     }
 
+    private fun loadTemplate(
+        file: File,
+        context: Map<String, String>
+    ): String {
+        return loadTemplate(file.toReader(), file.name, context)
+    }
+
+    private fun loadTemplate(
+        reader: Reader,
+        templateName: String,
+        context: Map<String, String>
+    ): String {
+        val runtimeServices = RuntimeSingleton.getRuntimeServices()
+        val simpleNode = runtimeServices.parse(reader, templateName)
+
+        val template = Template()
+        template.setRuntimeServices(runtimeServices)
+        template.data = simpleNode
+        template.initDocument()
+
+        val velocityContext = VelocityContext()
+        context.forEach { key, value ->
+            velocityContext.put(key, value)
+        }
+
+        val stringWriter = StringWriter()
+        template.merge(velocityContext, stringWriter)
+        return stringWriter.toString()
+    }
 
 }
 
 fun main(args: Array<String>) {
-    val chetiConfiguration = Cheti.loadConfiguration(args[0])
+    val chetiConfiguration = Cheti.loadConfiguration(args[0],
+        mapOf(
+            "IP_ADDRESS" to getLocalAddress(),
+            "HOSTNAME" to getHostName()
+        )
+    )
     Cheti.execute(chetiConfiguration)
 }
 
 fun getLocalAddress() =
-    InetAddress.getAllByName(InetAddress.getLocalHost().hostName).first { it.isSiteLocalAddress }.hostAddress
+    InetAddress.getAllByName(getHostName()).first { it.isSiteLocalAddress }.hostAddress
 
 fun getHostName() =
     InetAddress.getLocalHost().hostName
