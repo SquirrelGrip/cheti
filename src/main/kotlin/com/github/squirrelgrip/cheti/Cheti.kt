c8faf1d8de168cabf2d86648cddafedc6209dab7
Adrian <adrian.richter@gmail.com>
2020-04-07 14:42:50 +0800

    Added an easier executable option

--- src/main/kotlin/com/github/squirrelgrip/cheti/Cheti.kt
+++ src/main/kotlin/com/github/squirrelgrip/cheti/Cheti.kt
@@ -3,6 +3,7 @@ package com.github.squirrelgrip.cheti
 import com.github.squirrelgrip.cheti.configuration.ChetiConfiguration
 import com.github.squirrelgrip.extensions.json.toInstance
 import java.io.File
+import java.io.InputStream
 import java.net.InetAddress
 import java.security.KeyStore
 import java.security.PrivateKey
@@ -25,31 +26,51 @@ object Cheti {
 
     fun loadConfiguration(file: File): ChetiConfiguration {
         val chetiConfiguration = file.toInstance<ChetiConfiguration>()
+        validate(chetiConfiguration)
+        return chetiConfiguration
+    }
+
+    fun loadConfiguration(inputStream: InputStream): ChetiConfiguration {
+        val chetiConfiguration = inputStream.toInstance<ChetiConfiguration>()
+        validate(chetiConfiguration)
+        return chetiConfiguration
+    }
+
+    private fun validate(chetiConfiguration: ChetiConfiguration) {
         val errors = chetiConfiguration.validate()
         if (!errors.isEmpty()) {
             errors.forEach { println(it) }
             throw InvalidConfigurationException()
         }
-        return chetiConfiguration
+    }
+
+    fun execute(chetiConfiguration: ChetiConfiguration) {
+        validate(chetiConfiguration)
+        val certificateLoader = CertificateLoader(chetiConfiguration.common)
+        chetiConfiguration.certificates.forEach {
+            certificateLoader.load(it)
+        }
+        val chainLoader = ChainLoader(certificateLoader)
+        chetiConfiguration.chains.forEach {
+            chainLoader.load(it)
+        }
+
+        val keyStoreLoader = KeyStoreLoader(certificateLoader, chainLoader)
+        chetiConfiguration.keystores.forEach {
+            keyStoreLoader.load(it)
+        }
+
+    }
+
+    fun loadConfiguration(fileName: String): ChetiConfiguration {
+        return loadConfiguration(File(fileName))
     }
 
 }
 
 fun main(args: Array<String>) {
-    val chetiConfiguration = Cheti.loadConfiguration(File("cheti.json"))
-    val certificateLoader = CertificateLoader(chetiConfiguration.common)
-    chetiConfiguration.certificates.forEach {
-        certificateLoader.load(it)
-    }
-    val chainLoader = ChainLoader(certificateLoader)
-    chetiConfiguration.chains.forEach {
-        chainLoader.load(it)
-    }
-
-    val keyStoreLoader = KeyStoreLoader(certificateLoader, chainLoader)
-    chetiConfiguration.keystores.forEach {
-        keyStoreLoader.load(it)
-    }
+    val chetiConfiguration = Cheti.loadConfiguration(args[0])
+    Cheti.execute(chetiConfiguration)
 }
 
 fun getLocalAddress() =
